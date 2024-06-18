3a34679b33cca5e27e5c0a04058229439df13ca4
Nick Vigier <nvigier46@gmail.com>
2024-06-18 12:59:28 +0100

    Building, but Serverless Function now too large to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // experimental: {
+  //   outputFileTracingIncludes: {
+  //     '/api/test': ['/pw-browsers/**/*'],
+  //   },
+  // },
+};
 
 export default nextConfig;
--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,22 @@
+import { spawnSync } from 'child_process';
 import { NextResponse } from 'next/server';
 import { chromium } from 'playwright';
 
 export const dynamic = 'force-dynamic';
-
+// PLAYWRIGHT_BROWSERS_PATH=$HOME/pw-browsers
 export async function GET() {
-  const browser = await chromium.launch();
+  // spawnSync('ls', [
+  //   '-la',
+  //   `${process.cwd()}/node_modules/playwright-core/`,
+  // ]).output?.forEach((line) => {
+  //   console.log(line?.toString());
+  // });
+
+  const browser = await chromium.launch({
+    executablePath:
+      process.cwd() +
+      '/node_modules/.pnpm/playwright-core@1.44.1/node_modules/playwright-core/.local-browsers/chromium-1117',
+  });
   const context = await browser.newContext();
   const page = await context.newPage();
 
