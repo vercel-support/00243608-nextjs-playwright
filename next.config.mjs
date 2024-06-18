7dc136e35df45e7260b3aa7b5850d54862b447c4
Nick Vigier <nvigier46@gmail.com>
2024-06-18 15:23:10 +0100

    It works!

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   outputFileTracingIncludes: {
-  //     '/api/test': ['/pw-browsers/**/*'],
-  //   },
-  // },
+  experimental: {
+    serverComponentsExternalPackages: [
+      'playwright-core',
+      '@sparticuz/chromium',
+      'electron',
+    ],
+    // outputFileTracingIncludes: {
+    //   '/api/test': '@sparticuz/chromium'
+    // }
+  },
+  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+    // console.log('webpack', config.module.rules);
+    config.module.rules.push({
+      test: /\.(ttf|html)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url-loader",
+      options: {
+        limit: 10000,
+        mimetype: "application/octet-stream",
+        name: "fonts/[name].[ext]"
+      }
+    },
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,21 +1,13 @@
-import { spawnSync } from 'child_process';
 import { NextResponse } from 'next/server';
-import { chromium } from 'playwright';
+import { chromium as playwright } from 'playwright-core';
+import chromium from '@sparticuz/chromium';
 
 export const dynamic = 'force-dynamic';
-// PLAYWRIGHT_BROWSERS_PATH=$HOME/pw-browsers
-export async function GET() {
-  // spawnSync('ls', [
-  //   '-la',
-  //   `${process.cwd()}/node_modules/playwright-core/`,
-  // ]).output?.forEach((line) => {
-  //   console.log(line?.toString());
-  // });
 
-  const browser = await chromium.launch({
-    executablePath:
-      process.cwd() +
-      '/node_modules/.pnpm/playwright-core@1.44.1/node_modules/playwright-core/.local-browsers/chromium-1117',
+export async function GET() {
+  const browser = await playwright.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath(),
   });
   const context = await browser.newContext();
   const page = await context.newPage();
